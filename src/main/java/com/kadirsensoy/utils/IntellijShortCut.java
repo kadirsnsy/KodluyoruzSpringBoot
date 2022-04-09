f17560903be36f438a6cf25098ea4b96a10d0d5e
kadirsnsy <ksensoy@isbak.istanbul>
2022-04-09 17:00:57 +0300

    kısayollar

---
+++ src/main/java/com/kadirsensoy/utils/IntellijShortCut.java
@@ -0,0 +1,17 @@
+package com.kadirsensoy.utils;
+
+public class IntellijShortCut {
+
+    // **** Setting -> Keymap******
+ //   CTRL + ALT + S = settings (File=>> Settings)
+ //   ALT + INS => contructor,  toString , getter, setter
+ //   CTRL + / => single comments
+ //   CTRL + numlock / => single comments
+ //   CTRL + SHIFT + / => multiple comment
+ //   CTRL + SHIFT + numlock / => multiple comment
+ //   CTRL + K => commit
+
+
+
+
+}

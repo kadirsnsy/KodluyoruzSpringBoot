6a35c6a3872c4b216fe2a1f1a060d187c05ee72e
kadirsnsy <ksensoy@isbak.istanbul>
2022-04-09 17:01:37 +0300

    Devtools Aktif etmek

---
+++ src/main/java/com/kadirsensoy/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.kadirsensoy.utils;
+
+public class ConfigProgramming {
+    //DevTools => LiveReload
+    // File =>> Settings =>> Compiler =>> Build Project out comatically checked
+    // File =>> Settings =>> Advenced Settings =>> Compiler =>> Allow outo-make to start even if develloped application is currentyl running (selected)
+}

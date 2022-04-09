5e7c7c7ce0e4a392d0110b1530ee21d7ce617cb9
kadirsnsy <ksensoy@isbak.istanbul>
2022-04-09 16:59:37 +0300

    SpringBootApplication =>> Security (exclude)

--- src/main/java/com/kadirsensoy/KodluyoruzApplication.java
+++ src/main/java/com/kadirsensoy/KodluyoruzApplication.java
@@ -3,7 +3,11 @@ package com.kadirsensoy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//Security inactive
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class KodluyoruzApplication {
 
     public static void main(String[] args) {
